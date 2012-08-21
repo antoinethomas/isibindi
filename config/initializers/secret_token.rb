f8070257c8e965116d6ce4085f3eaa8d89f0b591
Antoine Thomas <antoine.thomas@gmail.com>
2012-08-21 22:05:38 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isibindi::Application.config.secret_token = 'ce2b06abf738efa47e5d6e411d5b40caf19ae1e7695793b62b1fb581e4cc244a3ad55df50cd0f50892e56049bac7fdd62328008df58f4419beaae2799220d3e4'
